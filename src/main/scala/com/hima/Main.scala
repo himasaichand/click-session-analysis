0310f4af98c4c7f259f41afd2528f4c4b7c4be63
himasaichand <102629864+himasaichand@users.noreply.github.com>
2023-01-11 23:00:03 +0000

    Update Main.scala

--- src/main/scala/com/hima/Main.scala
+++ src/main/scala/com/hima/Main.scala
@@ -1,29 +1,10 @@
 package com.hima
 
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+object Main  extends App{
 
+  println("hello")
+  System.setProperty("hadoop.home.dir","C:\\Users\\44758\\Downloads")
 
-object Main extends App {
-
-
-  if (args.length == 0) {
-    println(LocalDateTime.now())
-    val formatter = DateTimeFormatter.ofPattern("HH:mm")
-    val ht = formatter.format(LocalDateTime.now())
-    println(new humanFriendlyTransformation().human_transform(ht))
-  }
-  else {
-    if (args.length > 1) {
-      throw new IllegalArgumentException("Accepts one or none argument ")
-    }
-    else {
-
-      println(new humanFriendlyTransformation().human_transform(args(0)))
-    }
-
-
-  }
-
+  new TransformLoad()
 
 }
